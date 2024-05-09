0dc55a9c795778dd129321c27a78a12044715503
MaximTerekhin <152065729+MaximTerekhin@users.noreply.github.com>
2024-05-09 10:53:26 +0300

    gitognore

--- final_project/config.py
+++ final_project/config.py
@@ -1,9 +1,10 @@
-TOKEN_TELEGRAMM = ''
+TOKEN_TELEGRAMM = '6640595436:AAGQQGv3svaQ4IOLDrDRDZRE1LcJeRWRFd0'
 MAX_USERS_IN_DIALOG = 4
 
-SYSTEM_CONTENT = ''
+SYSTEM_CONTENT = ('Ты помощник по математике, физике, истории, биологии, химии, фнглийскому и русскому языкам.\n'                                                         
+                  'Отвечай кратко и понятно. Если видишь задачу от пользователя в виде (число+число) то выведи ответ тоже числом.')
 
-FOLDER_ID = ''
+FOLDER_ID = 'b1gnsaa5t2eoobepusc2'
 
 MAX_GPT_TOKENS_USER = 200000
 MAX_GPT_TOKENS_FOR_QUERE = 300
@@ -13,7 +14,7 @@ MAX_TTS_STT_TOKENS = 1500
 MAX_STT_BLOCKS = 50
 MAX_FOR_USER_TTS_STT_SYMBOL = 1000
 
-DATA_BASES_NAME = ''
-TABLE_NAME = ''
+DATA_BASES_NAME = 'Users.db'
+TABLE_NAME = 'Users_gpt'
 
 
